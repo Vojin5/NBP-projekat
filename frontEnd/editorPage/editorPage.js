a2510effb128fa0a1247d781fe8b1e5f2f802428
Vojin Minic <vojinminic@elfak.rs>
2024-01-06 18:30:27 +0100

    refactor

--- frontEnd/editorPage/editorPage.js
+++ frontEnd/editorPage/editorPage.js
@@ -2,56 +2,54 @@ import { baseUrl,serverUrl } from "../config.js";
 let quill;
 let oldContent = [null,null];
 let newContent = [];
-let currentOld = null;
-let currentNew = null;
 
 let documentID = localStorage["currentDocument"];
 
 let saveButton = document.getElementById("save");
 quill = new Quill("#editor", 
     {
-        theme: 'snow',
-        modules: {
-            toolbar: [
-              ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
-              ['blockquote', 'code-block'],
+      theme: 'snow',
+      modules: {
+          toolbar: [
+            ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+            ['blockquote', 'code-block'],
       
-              [{ 'header': 1 }, { 'header': 2 }],               // custom button values
-              [{ 'list': 'ordered'}, { 'list': 'bullet' }],
-              [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
-              [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
-              [{ 'direction': 'rtl' }],                         // text direction
+            [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+            [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+            [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+            [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+            [{ 'direction': 'rtl' }],                         // text direction
       
-              [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+            [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
       
-              [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
-              [{ 'font': [] }],
-              [{ 'align': [] }],
+            [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+            [{ 'font': [] }],
+            [{ 'align': [] }],
       
-              ['image'],
+            ['image'],
       
-              ['clean']                                         // remove formatting button
-            ],
-          },
-          placeholder: 'Compose an epic...',
-        }
-    );
+            ['clean']                                         // remove formatting button
+          ],
+        },
+        placeholder: 'Compose an epic...',
+      }
+  );
 
-    quill.on('text-change', function(delta, oldDelta, source) {
-      oldContent[0] = oldDelta;
-      oldContent[1] = delta;
-      newContent.push(delta);
-    });
+quill.on('text-change', function(delta, oldDelta, source) {
+    oldContent[0] = oldDelta;
+    oldContent[1] = delta;
+    newContent.push(delta);
+  });
 
-    saveButton.addEventListener("click",async() => {
-      const result = await fetch(serverUrl + `/Document/add-content/${documentID}`,
-      {
-        method:"POST",
-        headers:{
-          "Content-Type":"application/json"},
-        body:JSON.stringify(oldContent)
-      })
-    });
+saveButton.addEventListener("click",async() => {
+  const result = await fetch(serverUrl + `/Document/add-content/${documentID}`,
+    {
+      method:"POST",
+      headers:{
+        "Content-Type":"application/json"},
+      body:JSON.stringify(oldContent)
+    })
+  });
 
 
 
